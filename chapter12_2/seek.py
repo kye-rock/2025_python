4deb698ec4610802ecd3ab0a0405e631758adf8d
kye-rock <kangyeeun153@gmail.com>
2025-11-20 11:42:29 +0900

    2025.11.20

---
+++ chapter12_2/seek.py
@@ -0,0 +1,8 @@
+with open(r"C:\2025_2_python\chapter12_2\proverbs.txt", 'rb') as file:
+    file.seek(10, 0)
+
+    position = file.tell()
+    print("현재 파일 포인터 위치: ", position)
+
+    data = file.read(5)
+    print("읽은 데이터:", data)
